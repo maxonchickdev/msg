d45225fd3d815afe2ff7b309ba604ca0d393acb6
plinom <kutsenko.pn@ucu.edu.ua>
2024-06-07 20:44:46 +0300

    Some changes | commit

--- front-end/src/components/RequiredHandler.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const RequiredHandler = ({ content }: { content: string | undefined }) => {
-  return <p className='mt-[-6px] text-[10px] text-red-600'>{content}</p>
-}
-
-export default React.memo(RequiredHandler)
--- front-end/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { Routing } from './routes/Routing'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<Routing />)
