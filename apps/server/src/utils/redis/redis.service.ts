e5d7796b873a40c85aef4938d72b179c50287bf3
Max Kutsenko <maxondev@Maxs-MacBook-Pro.local>
2024-10-14 19:33:20 +0300

    Some changes | commit

--- apps/server/src/utils/redis/redis.service.ts
+++ apps/server/src/utils/redis/redis.service.ts
@@ -1,20 +1,20 @@
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Inject, Injectable } from '@nestjs/common';
-import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager'
+import { Inject, Injectable } from '@nestjs/common'
+import { Cache } from 'cache-manager'
 
 @Injectable()
 export class RedisService {
   constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
 
   async setValue(key: string, value: string): Promise<void> {
-    await this.cacheManager.set(key, value);
+    await this.cacheManager.set(key, value)
   }
 
   async getValue(key: string): Promise<string> {
-    return await this.cacheManager.get<string>(key);
+    return await this.cacheManager.get<string>(key)
   }
 
   async deleteValue(key: string): Promise<void> {
-    await this.cacheManager.del(key);
+    await this.cacheManager.del(key)
   }
 }
