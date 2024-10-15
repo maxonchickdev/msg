12441c45845834dedc7df643c37f235d5300de14
Max Kutsenko <maxondev@Maxs-MacBook-Pro.local>
2024-10-15 20:50:38 +0300

    Some changes | commit

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -8,16 +8,11 @@ const { composePlugins, withNx } = require('@nx/next')
  **/
 const nextConfig = {
   nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
     svgr: false,
   },
   output: 'standalone',
 }
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-]
+const plugins = [withNx]
 
 module.exports = composePlugins(...plugins)(nextConfig)
