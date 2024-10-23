b3585eda5559dab5b8c0c34e819f698ee55a4e00
Max Kutsenko <maxondev@Maxs-MacBook-Pro.local>
2024-10-23 21:52:22 +0300

    fix vercel.json file

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('docs', app, document, {
+  SwaggerModule.setup('/', app, document, {
     customSiteTitle: 'MSG api',
   });
 
